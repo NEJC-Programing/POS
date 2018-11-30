596b28b1836431f1654640bcafd6c9270bfa4c55
Noam Hadar <nhthebest.noam.hadar@gmail.com>
2018-12-01 09:26:16 +1300

    code

---
+++ UI/Keypad.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace UI
+{
+    partial class Keypad
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btnmin = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btnplus = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.buttonEner = new System.Windows.Forms.Button();
+            this.button00 = new System.Windows.Forms.Button();
+            this.button0 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn7
+            // 
+            this.btn7.Location = new System.Drawing.Point(3, 3);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(75, 75);
+            this.btn7.TabIndex = 0;
+            this.btn7.Text = "7";
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.Location = new System.Drawing.Point(84, 3);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(75, 75);
+            this.btn8.TabIndex = 0;
+            this.btn8.Text = "8";
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(3, 165);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(75, 75);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btnmin
+            // 
+            this.btnmin.Location = new System.Drawing.Point(246, 3);
+            this.btnmin.Name = "btnmin";
+            this.btnmin.Size = new System.Drawing.Size(75, 75);
+            this.btnmin.TabIndex = 0;
+            this.btnmin.Text = "-";
+            this.btnmin.UseVisualStyleBackColor = true;
+            this.btnmin.Click += new System.EventHandler(this.btnmin_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Location = new System.Drawing.Point(3, 84);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(75, 75);
+            this.btn4.TabIndex = 0;
+            this.btn4.Text = "4";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Location = new System.Drawing.Point(84, 84);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(75, 75);
+            this.btn5.TabIndex = 0;
+            this.btn5.Text = "5";
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.Location = new System.Drawing.Point(165, 84);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(75, 75);
+            this.btn6.TabIndex = 0;
+            this.btn6.Text = "6";
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            // 
+            // btnplus
+            // 
+            this.btnplus.Location = new System.Drawing.Point(246, 84);
+            this.btnplus.Name = "btnplus";
+            this.btnplus.Size = new System.Drawing.Size(75, 75);
+            this.btnplus.TabIndex = 0;
+            this.btnplus.Text = "+";
+            this.btnplus.UseVisualStyleBackColor = true;
+            this.btnplus.Click += new System.EventHandler(this.btnplus_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.Location = new System.Drawing.Point(165, 3);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(75, 75);
+            this.btn9.TabIndex = 0;
+            this.btn9.Text = "9";
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.btn9_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(84, 165);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(75, 75);
+            this.btn2.TabIndex = 0;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(165, 165);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(75, 75);
+            this.btn3.TabIndex = 0;
+            this.btn3.Text = "3";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // buttonEner
+            // 
+            this.buttonEner.Location = new System.Drawing.Point(246, 165);
+            this.buttonEner.Name = "buttonEner";
+            this.buttonEner.Size = new System.Drawing.Size(75, 156);
+            this.buttonEner.TabIndex = 0;
+            this.buttonEner.Text = "Enter";
+            this.buttonEner.UseVisualStyleBackColor = true;
+            this.buttonEner.Click += new System.EventHandler(this.buttonEner_Click);
+            // 
+            // button00
+            // 
+            this.button00.Location = new System.Drawing.Point(3, 246);
+            this.button00.Name = "button00";
+            this.button00.Size = new System.Drawing.Size(156, 75);
+            this.button00.TabIndex = 0;
+            this.button00.Text = "00";
+            this.button00.UseVisualStyleBackColor = true;
+            this.button00.Click += new System.EventHandler(this.button00_Click);
+            // 
+            // button0
+            // 
+            this.button0.Location = new System.Drawing.Point(165, 246);
+            this.button0.Name = "button0";
+            this.button0.Size = new System.Drawing.Size(75, 75);
+            this.button0.TabIndex = 0;
+            this.button0.Text = "0";
+            this.button0.UseVisualStyleBackColor = true;
+            this.button0.Click += new System.EventHandler(this.button0_Click);
+            // 
+            // Keypad
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.buttonEner);
+            this.Controls.Add(this.btnplus);
+            this.Controls.Add(this.btnmin);
+            this.Controls.Add(this.button0);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.button00);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Name = "Keypad";
+            this.Size = new System.Drawing.Size(325, 328);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btnmin;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btnplus;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button buttonEner;
+        private System.Windows.Forms.Button button00;
+        private System.Windows.Forms.Button button0;
+    }
+}
---
+++ UI/Keypad.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UI
+{
+    public partial class Keypad : UserControl
+    {
+        public Keypad()
+        {
+            InitializeComponent();
+        }
+    #region Events
+
+        public event KeyPressEventHandler ButtonPressed;
+
+        #endregion Events
+
+ 
+
+        #region Methods
+
+        public void RaiseButtonPressed(char WhatToSend)
+        {
+            KeyPressEventHandler handler = ButtonPressed;
+            if (handler != null)
+
+            {
+
+                handler(this, new KeyPressEventArgs(WhatToSend));
+
+            }
+        }
+
+        #endregion Methods
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            RaiseButtonPressed('7');
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            RaiseButtonPressed('8');
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            RaiseButtonPressed('9');
+        }
+
+        private void btnmin_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnplus_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void buttonEner_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            RaiseButtonPressed('6');
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            RaiseButtonPressed('5');
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            RaiseButtonPressed('4');
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            RaiseButtonPressed('1');
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            RaiseButtonPressed('2');
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            RaiseButtonPressed('3');
+        }
+
+        private void button0_Click(object sender, EventArgs e)
+        {
+            RaiseButtonPressed('0');
+        }
+
+        private void button00_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
